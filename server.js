9117f055c13ed09c2a29bcdaff0a6d5261c59424
bigez <rjones26@outlook.com>
2019-06-29 14:12:18 -0700

    remove route for port listener

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ app.post("/articles/:id", function(req, res) {
 });
 
 
-// Listen on port 3000
-app.listen(process.env.PORT || 3000, function() {
-  console.log("App running on port 3000!");
-});
+// // Listen on port 3000
+// app.listen(process.env.PORT || 3000, function() {
+//   console.log("App running on port 3000!");
+// });
